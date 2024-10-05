61738ddf2e23d1d685c9aace586404cfb9821e88
sstto <sspjyins@snu.ac.kr>
2024-10-06 00:36:02 +0900

    아이콘 회전 구현

--- app/src/main/java/com/wafflestudio/waffleseminar2024/main/TabLayoutActivity.kt
+++ app/src/main/java/com/wafflestudio/waffleseminar2024/main/TabLayoutActivity.kt
@@ -1,6 +1,8 @@
 package com.wafflestudio.waffleseminar2024.main
 
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -37,32 +39,35 @@ class TabLayoutActivity : AppCompatActivity() {
         }
 
         val tabs = listOf(
-            Tab.GameTab(this, "게임", AppCompatResources.getDrawable(this, R.drawable.ic_tab_game)),
-            Tab.AppTab(this, "앱", AppCompatResources.getDrawable(this, R.drawable.ic_tab_app)),
+            Tab.GameTab(this, "게임", AppCompatResources.getDrawable(this, R.drawable.ic_tab_game_selector)),
+            Tab.AppTab(this, "앱", AppCompatResources.getDrawable(this, R.drawable.ic_tab_app_selector)),
             Tab.SearchTab(searchViewModel, this, "검색", AppCompatResources.getDrawable(this, R.drawable.ic_tab_search)),
-            Tab.ProfileTab(this, "프로필", AppCompatResources.getDrawable(this, R.drawable.ic_tab_profile)),
+            Tab.ProfileTab(this, "프로필", AppCompatResources.getDrawable(this, R.drawable.ic_tab_profile_selector)),
         )
 
         viewPager = binding.viewPager.apply { adapter = TabLayoutViewAdapter(tabs) }
         tabLayout = binding.tabs.apply {
-//            addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
-//                override fun onTabSelected(tab: TabLayout.Tab) {
-//                   tab.customView?.apply {
-//                        findViewById<ImageView>(R.id.tab_icon).apply {
-//                            setTabIconTintResource(R.color.tab_color_selector)
-//                            setColorFilter(R.color.tab_color_selector)
-//                            setBackgroundResource(R.color.tab_background_color_selector)
-//                        }
-//                    }
-//                }
-//
-//                override fun onTabUnselected(tab: TabLayout.Tab?) {
-//                }
-//
-//                override fun onTabReselected(tab: TabLayout.Tab?) {
-//                }
-//
-//            })
+            addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab) {
+                    tab.customView?.apply {
+                        val icon = findViewById<ImageView>(R.id.tab_icon)
+                        val rotateAnimation = AnimationUtils.loadAnimation(this@TabLayoutActivity, R.anim.tab_selected)
+                        icon.startAnimation(rotateAnimation)
+                    }
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab) {
+                    tab.customView?.apply {
+                        val icon = findViewById<ImageView>(R.id.tab_icon)
+                        val rotateAnimation = AnimationUtils.loadAnimation(this@TabLayoutActivity, R.anim.tab_unselected)
+                        icon.startAnimation(rotateAnimation)
+                    }
+                }
+
+                override fun onTabReselected(tab: TabLayout.Tab) {
+                }
+
+            })
         }
 
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
